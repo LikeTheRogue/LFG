fa5c583f93b497e350ac95dd714775cae7159b13
Kyle Rogacion <30313930+LikeTheRogue@users.noreply.github.com>
2017-07-25 11:48:17 -0700

    Delete conf.lua

--- conf.lua
+++
@@ -1,9 +0,0 @@
-function love.conf(t)
-    t.title = "grid" -- The title of the window the game is in (string)
-    t.version = "0.10.2"         -- The LÖVE version this game was made for (string)
-    t.window.width = 640      -- we want our game to be long and thin.
-    t.window.height = 480
-
-    -- For Windows debugging
-    t.console = true
-end
